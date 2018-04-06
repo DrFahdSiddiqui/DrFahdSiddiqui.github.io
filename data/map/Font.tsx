f7085da0f49a3760da4613d2a010f27c85436c9f
Fahd Siddiqui <drfahdsiddiqui@gmail.com>
2018-04-05 19:36:50 -0500

    Delete Font.tsx

--- data/map/Font.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ResumeText" tilewidth="607" tileheight="114" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="607" height="114" source="../fnt/Content/Education.png"/>
- </tile>
-</tileset>
