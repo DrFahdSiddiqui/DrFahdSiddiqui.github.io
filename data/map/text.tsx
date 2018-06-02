abc629ac3b41fafb15862a7962f11e19b37e4b26
Fahd Siddiqui <drfahdsiddiqui@gmail.com>
2018-06-02 13:43:13 -0500

    Delete text.tsx

--- data/map/text.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="text" tilewidth="889" tileheight="178" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="889" height="178" source="../img/Text/L1IRoFS.png"/>
- </tile>
-</tileset>
